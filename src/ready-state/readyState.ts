86d925a178a97af67c890484578029b9b1585e4e
jeahaChoi <cjhmoves33@gmail.com>
2022-07-05 08:27:00 +0900

    feat: lint-staged 테스트

--- src/ready-state/readyState.ts
+++ src/ready-state/readyState.ts
@@ -4,4 +4,3 @@ document.addEventListener("readystatechange", asd => {
   console.log("dom ready state1 -> ", document.readyState);
   console.log("dom ready state1 -> ", document.readyState);
 });
-
