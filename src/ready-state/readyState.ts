acd1d3fd0b05dc426e3948d94e39eae635c474e7
jeahaChoi <cjhmoves33@gmail.com>
2022-07-05 08:58:00 +0900

    feat: preparing

--- src/ready-state/readyState.ts
+++ src/ready-state/readyState.ts
@@ -1,3 +1,7 @@
 document.addEventListener("readystatechange", asd => {
   console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
 });
+
