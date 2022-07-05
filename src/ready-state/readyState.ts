ba7bd9638d80994abadfaf8ba99948bae9e17f63
jeahaChoi <cjhmoves33@gmail.com>
2022-07-05 11:10:08 +0900

    test

--- src/ready-state/readyState.ts
+++ src/ready-state/readyState.ts
@@ -2,4 +2,6 @@ document.addEventListener("readystatechange", () => {
   console.log("dom ready state1 -> ", document.readyState);
   console.log("dom ready state1 -> ", document.readyState);
   console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
 });
