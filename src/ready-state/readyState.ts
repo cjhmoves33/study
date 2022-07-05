c1de65b819bdf50aca18b76a8259653e3312ae69
jeahaChoi <cjhmoves33@gmail.com>
2022-07-05 11:07:02 +0900

    husky test

--- src/ready-state/readyState.ts
+++ src/ready-state/readyState.ts
@@ -2,4 +2,9 @@ document.addEventListener("readystatechange", () => {
   console.log("dom ready state1 -> ", document.readyState);
   console.log("dom ready state1 -> ", document.readyState);
   console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
+  console.log("dom ready state1 -> ", document.readyState);
 });
