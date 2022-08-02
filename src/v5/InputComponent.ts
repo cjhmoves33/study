cc1d7cb9f2b5e1a84bb6817e1ce3ece1378e749c
jeahaChoi <cjhmoves33@gmail.com>
2022-08-03 08:19:20 +0900

    ..

--- src/v5/InputComponent.ts
+++ src/v5/InputComponent.ts
@@ -2,22 +2,14 @@ import { getValidationRule } from '@/module/module';
 import { ValidationType } from '@/module/types';
 
 export default class InputComponent extends HTMLElement {
-  // private wrapper = document.createElement('div');
-  // private label = document.createElement('label');
-  // private input = document.createElement('input');
-  // getValidationInputs('username')
-
-  private invalidMessageSpan = document.createElement('span');
-
   private ruleOption = this.getAttribute('validationRule') as ValidationType;
   private rule = getValidationRule(this.ruleOption);
-  // private rule!: ValidationRule;
 
   constructor() {
     super();
 
     const template = document.getElementById(
-      '#input-template'
+      'input-template'
     ) as HTMLTemplateElement;
     const templateContent = template.content;
 
@@ -25,21 +17,8 @@ export default class InputComponent extends HTMLElement {
     shadowRoot.appendChild(templateContent.cloneNode(true));
   }
 
-  private setHtml() {
-    // this.appendChild(this.wrapper);
-    // this.wrapper.appendChild(this.label);
-    // this.wrapper.appendChild(this.input);
-    // this.wrapper.appendChild(this.invalidMessageSpan);
-  }
-
-  private init() {
-    this.setHtml();
-  }
-
   connectedCallback() {
     if (this.isConnected) {
-      this.init();
-
       // this.rule = getValidationRule(this.ruleOption);
     }
   }
