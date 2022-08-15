3b3d07084a9b6c2902a36437d3eb67fb25e9cd09
jeahaChoi <jaeha-choi@nhn-commerce.com>
2022-08-15 18:38:33 +0900

    foo: foo
    Release-As: 2.0.5

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  value: [
-    'chore',
-    'feat',
-    'fix',
-    'test'
-  ]
+  extends: ['@commitlint/config-conventional']
 };
